26a9666467ba323cafc5bd54268e87ed2135332a
Daymos1996 <38887719+Daymos1996@users.noreply.github.com>
2019-01-17 12:13:23 +0100

    Initial commit

---
+++ app/src/main/java/daymos/lodz/uni/math/pl/memorygame/MainActivity.java
@@ -0,0 +1,35 @@
+package daymos.lodz.uni.math.pl.memorygame;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button start;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        start = (Button) findViewById(R.id.start);
+
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(intent);
+
+
+            }
+        });
+
+    }
+
+
+
+
+}
